cd5c85821a8ea5d593bf5e0dbdd8a944e19ab65b
manuelchopourian <manuelchopourian@Manuels-MacBook-Air.local>
2020-06-12 15:30:27 -0300

    buscador terminado (faltan errores)

--- js/results.js
+++ js/results.js
@@ -1,17 +1,28 @@
-let results = document.querySelector('.search-results')
+let tracks = document.querySelector('.tracks-search-results');
+let artist = document.querySelector('.artist-search-results');
+let album = document.querySelector('.album-search-results');
 
 var queryString = location.search;
 var queryStringObj = new URLSearchParams(queryString);
 
 let busqueda = queryStringObj.get('search');
-console.log(busqueda)
+console.log(busqueda);
 
 let proxy = "https://cors-anywhere.herokuapp.com/";
-let url = proxy + 'https://api.deezer.com/search?q=' + busqueda;
+let urlTrack = proxy + 'https://api.deezer.com/search/track?q=' + busqueda;
+let urlArtist = proxy + 'https://api.deezer.com/search/artist?q=' + busqueda;
+let urlAlbum = proxy + 'https://api.deezer.com/search/album?q=' + busqueda;
 
-console.log(url)
+let spinner = document.querySelector('.spinner');
+let content = document.querySelector('.search')
 
-fetch(url)
+window.onload = function() {
+    spinner.style.display = 'none';
+    content.style.display = 'grid'
+
+ }
+
+fetch(urlTrack)
     .then(function(response){
         return response.json();
     })
@@ -19,7 +30,7 @@ fetch(url)
         let resultados = datos.data;
        
         resultados.forEach(function(resultado){
-            results.innerHTML += '<li class="info">' + '<img src="' + resultado.album.cover +'" class="portadas">' +'<a href="detail.html?id=' +  resultado.id + '">' + resultado.title_short + '</a></li>'     
+            tracks.innerHTML += '<li class="info">' + '<img src="' + resultado.album.cover +'" class="portadas">' +'<a href="detail.html?id=' +  resultado.id + '">' + resultado.title_short + '</a></li>';
         });
         console.log(resultados)   
         })
@@ -27,3 +38,36 @@ fetch(url)
     .catch(function(error){
         console.log(error);
     })
+    fetch(urlArtist)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        let resultados = datos.data;
+       
+        resultados.forEach(function(resultado){
+            artist.innerHTML +='<li class="info">' + '<img src="' + resultado.picture +'" class="portadas">' +'<a href="detail.html?id=' +  resultado.id + '">' + resultado.name + '</a></li>'; 
+        });
+        console.log(resultados)   
+        })
+
+    .catch(function(error){
+        console.log(error);
+    })
+    fetch(urlAlbum)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        let resultados = datos.data;
+       
+        resultados.forEach(function(resultado){
+            album.innerHTML +='<li class="info">' + '<img src="' + resultado.cover +'" class="portadas">' +'<a href="detail.html?id=' +  resultado.id + '">' + resultado.title + '</a></li>'; 
+        });
+        console.log(resultados)   
+        })
+
+    .catch(function(error){
+        console.log(error);
+    })
+   
