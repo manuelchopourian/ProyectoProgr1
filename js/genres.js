db25482385e6552f8475d75b48aa92df84d4a6d8
manuelchopourian <manuelchopourian@Manuels-MacBook-Air.local>
2020-06-16 17:24:07 -0300

    genre casi terminado

---
+++ js/genres.js
@@ -0,0 +1,18 @@
+let proxy = "https://cors-anywhere.herokuapp.com/";
+let url = proxy + 'https://api.deezer.com/genre';
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        let generos = document.querySelector('.generos');
+        let resultados = datos.data;
+        console.log(resultados)
+        resultados.forEach(function(genre){
+            generos.innerHTML += '<li class="info">' + '<a href="detail.html?id=' +  genre.id + '">' +  '<img src="' + genre.picture_big +'" class="portadas">' + '</a>'  +'<a href="detail.html?id=' +  genre.id + '" class="nombre">' + genre.name + '</a></li>'
+        })
+    })
+    .catch(function(error){
+        console.log(error)
+    })
