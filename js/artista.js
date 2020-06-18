77e2e683d9471a2bf35d4961ea1472b0c30c340b
andycheb <andy.cheb@hotmail.com>
2020-06-17 21:37:59 -0300

    arrrtista, ver q onda lo de foto

--- js/artista.js
+++ js/artista.js
@@ -1,10 +1,15 @@
 let queryString = new URLSearchParams(location.search);
-    let codigoArtist = queryString.get('id')
+
+let codigoArtist = queryString.get('id')
+let codigoFans = queryString.get('id')
+let codigoFoto = queryString.get('url')
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
 let url = proxy + 'https://api.deezer.com/artist/';
 
 let artistas = url + codigoArtist
+let fans = url + codigoFans
+let foto = url + codigoFoto
 
 fetch(artistas)
     .then(function(response){
@@ -16,12 +21,25 @@ fetch(artistas)
         h2.innerHTML += resultado.name
     })
 
-    fetch(seguidores)
+
+
+    fetch(fans)
     .then(function(response){
         return response.json();
     })
     .then(function(resultado){
         let nombre = resultado.data
         let h3 = document.querySelector("h3")
-        h3.innerText += resultado.nb_fan
+        h3.innerHTML += resultado.nb_fan + " Seguidores"
+    })
+
+    
+    fetch(foto)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(resultado){
+        let nombre = resultado.data
+        let fotoArtista = document.querySelector("fotoArtista")
+        fotoArtista.innerHTML += resultado.picture_medium
     })
